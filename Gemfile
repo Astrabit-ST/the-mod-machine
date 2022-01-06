d032a2b118e00518180e327f8703a1a6c444b2ac
Speak2Erase <matthew@nowaffles.com>
2022-01-06 13:35:26 -0800

    Add markdown parser

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+# Redcarpet markdown parser
+gem "redcarpet", "~> 3.5.1"
+
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+    def to_markdown(text)
+        extensions = {
+            autolink:           true,
+            superscript:        true,
+            underline: true,
+            strikethrough: true,
+            highlight: true,
+            superscript: true,
+            space_after_headers: true,
+            disable_indented_code_blocks: true,
+            fenced_code_blocks: true,
+            tables: true,
+        }
+    
+        renderer = ::Redcarpet::Render::HTML.new
+        markdown = ::Redcarpet::Markdown.new(renderer, extensions)
+        markdown.render(text.to_s)
+    end    
 end
